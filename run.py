490c027b685652f1c07401001a11bd63b88d8ca5
lawson <lawson@192.168.1.9>
2026-09-12 16:32:46 +0800

    update

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ from app.seed import run_seed
 
 app = create_app()
 
-with app.app_context():
-    run_seed()
+# with app.app_context():
+#     run_seed()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
